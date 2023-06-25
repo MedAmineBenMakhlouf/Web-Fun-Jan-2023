20f0efee6ac52453a6bf95ef3b18099328f29f1a
Mohamed Amine Ben Makhlouf <mohamedamin.benmakhlouf@gmail.com>
2023-06-26 00:09:38 +0200

    update dojo library

--- Week2/Day01/Practice/VideoPreview/script.js
+++ Week2/Day01/Practice/VideoPreview/script.js
@@ -1,1 +1,13 @@
-console.log("page loaded...");
+function playStop()
+{
+    vid = document.getElementById('vid');
+    if(vid.play)
+    {
+        vid.controls.stop;
+    }
+    else
+    {
+        vid.autoplay=true;
+        vid.muted=false;
+    }
+}

0f65de97c7f031bb8ff63a99fb8aa53af168eea5
MohamedAmineBenMakhlouf <mohamedamin.benmakhlouf@gmail.com>
2023-07-03 16:43:21 +0100

    W3D1DemoBelt

---
+++ Week3/Day01/script.js
@@ -0,0 +1,20 @@
+function remove(e)
+{
+    e.remove();
+}
+function voteUp(id)
+{
+    document.getElementById(id).innerText++
+}
+
+function voteDown(id)
+{
+    document.querySelector(`#${id}`).innerText--;
+}
+
+function find()
+{
+    inputValue = document.getElementById('search').value;
+    alert(`Searching for: "${inputValue}"`);
+    document.getElementById('search').value="";
+}
